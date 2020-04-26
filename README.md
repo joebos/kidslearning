2067f142bb112d514253b76b43314c1d296258be
Joe MacBookPro15 <joe@socialsellingjoy.com>
2020-04-26 18:48:27 -0400

    ...

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+pip install -e git+git@github.com:joebos/kidslearning.git#egg=kidslearning
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+
+    name="KidsLearning",
+    version="0.0.6",
+    description="Libraries for kids learning",
+    author="joebos",
+    author_email="joeleeboston@gmail.com",
+    url="https://github.com/joebos/kidslearning",
+    packages = ['kidslearning'],
+    license = "LICENSE",
+
+    install_requires = [
+    ],
+
+    tests_require=[
+    ]
+
+)
